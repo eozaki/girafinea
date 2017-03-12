615dc5bdb75511a492a2a54fc1aaf576645cfc70
Erick Cordeiro Ozaki <erick@jaya.tech>
2017-03-12 15:27:55 -0300

    adding flash messages to layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  $('.close-flash-message').on('click', function() {
+    $('.alert').fadeOut();
+  });
+});
