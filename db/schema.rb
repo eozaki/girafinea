ce9e22be2e36756f589d761a043c13ed5acd4f78
Erick Cordeiro Ozaki <erick@jaya.tech>
2017-03-26 17:48:02 -0300

    Adding description and start_time columns to Event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318205035) do
+ActiveRecord::Schema.define(version: 20170326204748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20170318205035) do
     t.integer  "user_id"
     t.string   "title"
     t.datetime "time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.datetime "start_time"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
