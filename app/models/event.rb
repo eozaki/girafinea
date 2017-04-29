832f0f6799150b3c31387994a7ccebf278f30009
Erick Cordeiro Ozaki <erick@jaya.tech>
2017-04-29 15:52:35 -0300

    Using cocoon for multiple entries on Items form

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,5 @@ class Event < ActiveRecord::Base
   has_many :items
 
   validates :title, :description, :start_time, presence: true
-  accepts_nested_attributes_for :items
+  accepts_nested_attributes_for :items, reject_if: :all_blank, allow_destroy: true
 end
